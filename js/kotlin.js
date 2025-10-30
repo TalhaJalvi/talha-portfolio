8f0db22f3d17c467e3b40c5d9ab56a1f8ccca83c
Talha Abbas Jalvi <talhajalvi321@gmail.com>
2025-10-30 14:53:59 +0500

    After making changes

--- js/kotlin.js
+++ js/kotlin.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const contentDisplay = document.querySelector('.content #content-display'); // Target div where content is loaded
     const menuToggle = document.getElementById('menu-toggle');
     const sidebar = document.querySelector('.sidebar');
-    // const navLinks = document.querySelectorAll('.sidebar .nav-link');
+    const navLinks = document.querySelectorAll('.sidebar .nav-link');
     const themeSwitcher = document.getElementById('theme-switcher');
     const body = document.body;
 
